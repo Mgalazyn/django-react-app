4c524ccbe636f37074c096accfc15a441eef7e66
Mgalazyn <m.galazynn@gmail.com>
2023-03-12 21:15:01 +0100

    .sd

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import Login from "./Login";
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Timer />);
 
 const App = () => {
   const [unauthorized, setUnauthorized] = useState(false);
