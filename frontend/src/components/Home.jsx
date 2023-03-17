97ffa5fef549af613c0fdfa0fbe08687cb5245a5
Mgalazyn <m.galazynn@gmail.com>
2023-03-17 22:13:54 +0100

    front

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const left = new URL("./images/left.jpg", import.meta.url)
+const left2 = new URL("./images/left.jpg", import.meta.url)
 const main = new URL("./images/main.jpg", import.meta.url)
-const right = new URL("./images/right.jpg", import.meta.url)
-const right2= new URL("./images/test2.jpg", import.meta.url)
-const left2 = new URL("./images/test1.jpg", import.meta.url)
+const left = new URL("./images/left1.jpg", import.meta.url)
+const test2= new URL("./images/test2.jpg", import.meta.url)
+const test1 = new URL("./images/test1.jpg", import.meta.url)
 
 export default function Home() {
     return (
@@ -13,11 +13,11 @@ export default function Home() {
                 <div className="content">
                     <div className="Hero">
                         <div className="hero-images">
-                            <img src={right} alt='right' />  
-                            <img src={left} alt='left'/>
+                            <img src={left} alt='left' />  
+                            <img src={left2} alt='left2'/>
                             <img src={main} alt ='main'/>         
-                            <img src={left2} alt='test1' />
-                            <img src={right2} alt='right2' />  
+                            <img src={test1} alt='test1' />
+                            <img src={test2} alt='test2' />  
                         </div>
                     </div>
                 </div>
